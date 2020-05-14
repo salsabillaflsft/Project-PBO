79f6b9316f6e62ae2ffa3b8916ce4714131ab60c
salsabillaflsft <50274165+salsabillaflsft@users.noreply.github.com>
2020-05-14 21:46:14 +0700

    sql

--- src/restaurant.sql
+++ src/restaurant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2020 at 03:45 PM
+-- Generation Time: May 14, 2020 at 04:45 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -21,6 +21,76 @@ SET time_zone = "+00:00";
 --
 -- Database: `restaurant`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `kategori` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`kategori`) VALUES
+('Bumbu'),
+('Bumbu Kering'),
+('Daging'),
+('Minuman'),
+('Sayur'),
+('Seafood');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `supplier` varchar(100) NOT NULL,
+  `contact` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`supplier`, `contact`) VALUES
+('PT.ABC', '0980765433210'),
+('PT.FILSA', '081123456789\r\n'),
+('PT.HANI', '081928374654'),
+('PT.QWERTY', '081122334455'),
+('PT.SALSA', '081329929929'),
+('PT.XYZ', '081234567890');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supply`
+--
+
+CREATE TABLE `supply` (
+  `id_supply` int(10) NOT NULL,
+  `kategori` varchar(100) NOT NULL,
+  `supplier` varchar(100) NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `jumlah_produk` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `supply`
+--
+
+INSERT INTO `supply` (`id_supply`, `kategori`, `supplier`, `nama_produk`, `jumlah_produk`) VALUES
+(1, 'Minuman', 'PT.HANI', 'Ocha', 100),
+(2, 'Bumbu', 'PT.XYZ', 'Soy Sauce', 50),
+(3, 'Seafood', 'PT.SALSA', 'Ikan Salmon', 100),
+(4, 'Sayur', 'PT.FILSA', 'Sayur Wortel', 50),
+(5, 'Bumbu', 'PT.ABC', 'Cabai', 100);
 
 -- --------------------------------------------------------
 
@@ -46,10 +116,51 @@ INSERT INTO `user` (`id_user`, `username`, `password`) VALUES
 --
 
 --
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`kategori`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`supplier`);
+
+--
+-- Indexes for table `supply`
+--
+ALTER TABLE `supply`
+  ADD PRIMARY KEY (`id_supply`),
+  ADD KEY `fk_kategori` (`kategori`),
+  ADD KEY `fk_supplier` (`supplier`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `supply`
+--
+ALTER TABLE `supply`
+  MODIFY `id_supply` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `supply`
+--
+ALTER TABLE `supply`
+  ADD CONSTRAINT `fk_kategori` FOREIGN KEY (`kategori`) REFERENCES `kategori` (`kategori`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_supplier` FOREIGN KEY (`supplier`) REFERENCES `supplier` (`supplier`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
