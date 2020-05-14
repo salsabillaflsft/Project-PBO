26c0f761772099583c7ac4a04e874a376a2b90a4
hani1206 <55271058+hani1206@users.noreply.github.com>
2020-05-14 10:04:09 +0700

    tengokk

--- src/Controller/ControllerAddSupply.java
+++ src/Controller/ControllerAddSupply.java
@@ -1,27 +1,32 @@
 package Controller;
 
 import MVC.MVC_Home;
+import Model.ModelKategori;
 import Model.ModelSupply;
 import View.ViewAddSupply;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class ControllerAddSupply {
     ViewAddSupply viewAddSupply;
     ModelSupply modelSupply;
+    ModelKategori modelKategori;
 
     public ControllerAddSupply(ViewAddSupply viewAddSupply,  ModelSupply modelSupply){
         this.viewAddSupply = viewAddSupply;
         this.modelSupply = modelSupply;
+        this.modelKategori = modelKategori;
 
-       /* if (modelSupply.getBanyakData2() != 0){
+        if (modelSupply.getBanyakData2() != 0){
             ArrayList<String> readNamaKategori = modelSupply.readNamaKategori();
-            //viewAddSupply.getKategori(readNamaKategori,viewAddSupply.namaKolom).getModel());
+            viewAddSupply.table.setModel(new JTable(readNamaKategori, viewAddSupply.namaKolom)).getModelkategori());
             updateDataCombo(modelSupply.readNamaKategori());
         } else {
             JOptionPane.showMessageDialog(null, "Data Masih Kosong");
-        }*/
+        }
 
         viewAddSupply.btnHome.addActionListener(new ActionListener() {
             @Override
@@ -47,12 +52,29 @@ public class ControllerAddSupply {
                 modelSupply.insertSupply(nama_produk,jumlah_produk);
             }
         });
+        viewAddSupply.btnAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                 if (e.getSource() == viewAddSupply.btnAdd){
+                    String no_hp = viewAddSupply.getKategori();
+                 } else {
+                     //viewAddSupply.tfNama2.setText("");
+
+                 }
+                String nama_produk = viewAddSupply.getSupply();
+                String jumlah_produk = viewAddSupply.getJumlah();
+                modelSupply.insertSupply(nama_produk,jumlah_produk);
+            }
+        });
+
     }
-    /*
+
+
+
     private void updateDataCombo(ArrayList<String> readNamaKategori) {
-        viewAddSupply.getKategori().removeAllItems();
+        viewAddSupply.cbKategori.removeAllItems();
         for (String item : readNamaKategori) {
-            viewAddSupply.getKategori().addItem(item);
+            viewAddSupply.cbKategori.addItem(item);
         }
-    }*/
+    }
 }
